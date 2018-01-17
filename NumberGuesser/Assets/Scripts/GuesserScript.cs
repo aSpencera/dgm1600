c279e64609e5b29df68f47c2f8e01d808a3ac0ed
Unknown <lastof6.boys@gmail.com>
2018-01-17 10:31:20 -0700

    Added Up and Down Arrow Comands

--- NumberGuesser/Assets/Scripts/GuesserScript.cs
+++ NumberGuesser/Assets/Scripts/GuesserScript.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuesserScript : MonoBehaviour {
+public class GuesserScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        print("Our game has started!");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int max;
+    public int min;
+    public int guess = 107;
+
+    // Use this for initialization
+    private void Start()
+    {
+        print("Welcome to Number Guesser");
+        print("Pick a number between " + min + " and " + max);
+
+        //Is the value Guess
+        print("Is the number 500?");
+        //Instructions - push these buttons
+        print("Up arrow for higher, Down arrow for lower, Enter for correct.");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            min = guess;
+            guess = (min + max) / 2;
+            print("Is the number" + guess + "?");
+        }
+
+        //up arrow
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (min + max) / 2;
+            print("Is the number" + guess + "?");
+        }
+
+        //Enter button
+        if (Input.GetKey(KeyCode.KeypadEnter))
+        { }
+
+
+    }
 }
