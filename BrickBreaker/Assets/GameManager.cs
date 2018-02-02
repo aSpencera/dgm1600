adaeaafb7e2b7d6c11ea43f75dc0023cb31e2d8a
Unknown <lastof6.boys@gmail.com>
2018-02-02 10:30:23 -0700

    Created Menu for Brick Breaker

---
+++ BrickBreaker/Assets/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBreaker : MonoBehaviour {
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+
+    }
+}
