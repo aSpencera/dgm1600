f62578a81f5dd1c923305bc09b4044bd60cfa54b
Unknown <lastof6.boys@gmail.com>
2018-02-12 10:38:06 -0700

    Added Paddle Script

---
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+
+	
+	void Update ()
+    {
+        //get the current paddle position
+        Vector3 paddlePosition = new Vector3(0, gameObject.transform.position.y, 0);
+
+        //get horizontal position 
+        float mousePositionInBlocks = Input.mousePosition.x / Screen.width * 16 - 8;
+
+        paddlePosition.x = mousePositionInBlocks;
+
+        //set this paddle object to saved position
+        this.gameObject.transform.position = paddlePosition;
+	}
+}
