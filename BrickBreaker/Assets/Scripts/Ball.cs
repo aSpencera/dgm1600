1c0659648ee0c9d351563236472d2ec6bfc7a414
Unknown <lastof6.boys@gmail.com>
2018-02-16 09:58:54 -0700

    Created Game Over level

--- BrickBreaker/Assets/Scripts/Ball.cs
+++ BrickBreaker/Assets/Scripts/Ball.cs
@@ -12,4 +12,6 @@ public class Ball : MonoBehaviour {
         AudioSource.PlayClipAtPoint(hitSound, collision.transform.position);
     }
 
+
+
 }
--- BrickBreaker/Assets/Scripts/LevelManager.cs
+++ BrickBreaker/Assets/Scripts/LevelManager.cs
@@ -5,6 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+
+
+    public static LevelManager instance = null;
+
+
+    void Awake()
+    {
+        //Singleton********
+
+        if (instance == null)                               //if instance is not assigned
+        {
+            instance = this;
+        }
+        else if (instance != this)                          //then assign instance to this
+        {
+            Destroy(this.gameObject);                       //then destroy this object
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+
     public void LoadLevel(string level)
     {
         SceneManager.LoadScene(level);
