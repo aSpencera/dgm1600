6221fcf7eaa964d9ec375235e12a3a0ee2670fd7
Unknown <lastof6.boys@gmail.com>
2018-03-12 10:46:09 -0600

    Added more spawn objects and AstroMan

---
+++ Scripting Slackers Origin/Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    public ParticleSystem deathParticle;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collided");
+        health--;
+        if (health <= 0)
+        {
+            Instantiate(deathParticle);
+            Destroy(gameObject);
+        }
+    }
+}
