2c4dd354665db743a3f7e884f1149de0bc624118
Unknown <lastof6.boys@gmail.com>
2018-03-28 10:03:12 -0600

    Made SwitchStatement and Started Blupee Money

---
+++ Scripting Slackers Origin/Assets/Scripts/CoinCollector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollector : MonoBehaviour {
+
+    public Text countText;
+    public int coinNum;
+
+    void Start()
+    {
+        coinNum = 0;
+        countText.text = "Count: " + coinNum.ToString();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Blupee")
+        {
+            other.gameObject.SetActive(false);
+            coinNum++;
+            countText.text = "Count: " + coinNum.ToString();
+        }
+    }
+
+    void Update()
+    {
+        countText.text = "Count: " + coinNum;
+    }
+}
---
+++ Scripting Slackers Origin/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class PowerUp : MonoBehaviour {
+
+
+    public enum Power { ScoreBoost, DoubleCoin};
+    public Power powerupType;
+    public SpriteRenderer rend;
+    public Sprite[] images;
+
+
+
+	private void Start ()
+    {
+        rend = GetComponent<SpriteRenderer>();
+		
+	}
+
+    private void Update()
+    {
+        switch (powerupType)
+        {
+            case Power.ScoreBoost:
+                rend.sprite = images[0];
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+
+}
