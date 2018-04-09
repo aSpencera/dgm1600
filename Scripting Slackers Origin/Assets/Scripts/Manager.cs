32d891948126835dfbd880944b00ff4d89a4798a
Unknown <lastof6.boys@gmail.com>
2018-04-08 22:19:50 -0600

    Extened the Joyride levels added the metispin lvl

--- Scripting Slackers Origin/Assets/Scripts/Manager.cs
+++ Scripting Slackers Origin/Assets/Scripts/Manager.cs
@@ -8,12 +8,16 @@ public class Manager : MonoBehaviour {
 
     public Text scoreBoard;
     private int score;
+	public int badies;
 
 
     public void IncrementScore(int value)
     {
         score += value;
         scoreBoard.text = score.ToString();
+
+		badies += value;
+		scoreBoard.text = badies.ToString();
     }
 
 	
